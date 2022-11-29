b5eae8ed4d437e2005200af122e9b0cafa2e24eb
Chris Khiev <chrissnakhiev@gmail.com>
2022-11-29 13:49:37 -0500

    added style folder for use

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,7 +3,18 @@ import React from "react";
 const Home = () => {
   return (
     <div className="container">
-      <h1>HELP</h1>
+      <section className="one">
+        <h1>1</h1>
+      </section>
+      <section className="two">
+        <h1>2</h1>
+      </section>
+      <section className="three">
+        <h1>3</h1>
+      </section>
+      <section className="four">
+        <h1>4</h1>
+      </section>
     </div>
   );
 };
