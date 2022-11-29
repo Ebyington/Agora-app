56aeeaf6186736c31767f0c2d21f3cc64e31c666
Chris Khiev <chrissnakhiev@gmail.com>
2022-11-28 19:17:20 -0500

    fixed develop for now

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
 
-// const { typeDefs, resolvers } = require('./schemas');
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
@@ -41,6 +41,6 @@ const startApolloServer = async (typeDefs, resolvers) => {
   })
  };
   
-  // Call the async function to start the server
-  startApolloServer(typeDefs, resolvers);
+// Call the async function to start the server
+startApolloServer(typeDefs, resolvers);
 
