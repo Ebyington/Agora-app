1f7f908ef6686f588c059b0f0ddaf7cd619138f7
William <110736589+ConnorMurphy9@users.noreply.github.com>
2022-11-30 17:49:48 -0500

    added logo to nav bar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import omnv from '../assets/logo-omnv.png';
 
 function Navbar (props) {
   
@@ -11,6 +12,7 @@ function Navbar (props) {
         <header className="dark:bg-gray-800 dark:text-white text-black md:sticky top-0 z-10">
              <ul>
                 <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+                <img className="image2" src={omnv} alt="OMNV logo" width="300" height="600"/>
                     <div className="title-font font-medium text-white mb-4 md:mb-0">
                         <li className="inline-flex items-center dark:bg-gray-800 dark:text-white text-black border-0 py-1 px-3 focus:outline-none hover:bg-indigo-600 rounded text-base mt-4 md:mt-0 cursor-pointer">
                             <Link to ="/">Home</Link>
