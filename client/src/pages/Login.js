ca7da8795b4e0463632e2f8a1a25aa9958b457d3
Stiicer <diegoandremora@gmail.com>
2022-12-01 10:25:28 -0500

    login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -11,10 +11,10 @@ function Login(props) {
   const formData = async (event) => {
     event.preventDefault();
     try {
-      const mutationResponse = await login({
+      const loginMutation = await login({
         variables: { email: formState.email, password: formState.password },
       });
-      const token = mutationResponse.data.login.token;
+      const token = loginMutation.data.login.token;
       Auth.login(token);
     } catch (e) {
       console.log(e);
