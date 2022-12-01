d943fbc46700dadbfc2eb96fe7f367ac6d409a4a
Stiicer <diegoandremora@gmail.com>
2022-12-01 11:45:47 -0500

    added ref review to product schema

--- server/models/index.js
+++ server/models/index.js
@@ -2,6 +2,7 @@ const User = require('./User');
 const Product = require('./Product');
 const History = require('./History');
 const Category = require('./Category');
+const Review = require('./Review')
 
 
-module.exports = { User, Product, History, Category };
+module.exports = { User, Product, History, Category, Review };
