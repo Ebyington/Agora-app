80f29f673825d5c462eb300a39102748ae8f285f
Eric Byington <112353490+Ebyington@users.noreply.github.com>
2022-11-30 20:50:54 -0500

    css buttons fit pallete

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,30 +6,28 @@ function Navbar (props) {
   
   return (
     <>
-      <h1 className="bg-black text-6xl font-normal leading-normal mt-0 mb-2 text-red-900">
-        OMNV- join the movement
-      </h1>
-        <header className="dark:bg-gray-800 dark:text-white text-black md:sticky top-0 z-10">
+     
+        <header className="dark:text-white text-white md:sticky top-0 z-10" >
              <ul>
                 <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
                 <img className="image2" src={omnv} alt="OMNV logo" width="300" height="600"/>
                     <div className="title-font font-medium text-white mb-4 md:mb-0">
-                        <li className="inline-flex items-center dark:bg-gray-800 dark:text-white text-black border-0 py-1 px-3 focus:outline-none hover:bg-indigo-600 rounded text-base mt-4 md:mt-0 cursor-pointer">
+                        <li className="inline-flex items-center dark:bg-gray-800 dark:text-white text-white border-0 py-1 px-3 focus:outline-none rounded text-base mt-4 md:mt-0 cursor-pointer">
                             <Link to ="/">Home</Link>
                         </li>
-                        <li className="inline-flex items-center dark:bg-gray-800 dark:text-white text-black border-0 py-1 px-3 focus:outline-none hover:bg-indigo-600 rounded text-base mt-4 md:mt-0 cursor-pointer">
+                        <li className="inline-flex items-center dark:bg-gray-800 dark:text-white text-white border-0 py-1 px-3 focus:outline-none rounded text-base mt-4 md:mt-0 cursor-pointer">
                             <Link to ="/Login">Login</Link>
                         </li>
-                        <li className="inline-flex items-center dark:bg-gray-800 dark:text-white text-black border-0 py-1 px-3 focus:outline-none hover:bg-indigo-600 rounded text-base mt-4 md:mt-0 cursor-pointer">
+                        <li className="inline-flex items-center dark:bg-gray-800 dark:text-white text-white border-0 py-1 px-3 focus:outline-none rounded text-base mt-4 md:mt-0 cursor-pointer">
                             <Link to ="/Signup">Signup</Link>
                         </li>
-                        <li className="inline-flex items-center dark:bg-gray-800 dark:text-white text-black border-0 py-1 px-3 focus:outline-none hover:bg-indigo-600 rounded text-base mt-4 md:mt-0 cursor-pointer">
+                        <li className="inline-flex items-center dark:bg-gray-800 dark:text-white text-white border-0 py-1 px-3 focus:outline-none rounded text-base mt-4 md:mt-0 cursor-pointer">
                             <Link to ="/Products">Products</Link>
                         </li>
-                        <li className="inline-flex items-center dark:bg-gray-800 dark:text-white text-black border-0 py-1 px-3 focus:outline-none hover:bg-indigo-600 rounded text-base mt-4 md:mt-0 cursor-pointer">
+                        <li className="inline-flex items-center dark:bg-gray-800 dark:text-white text-white border-0 py-1 px-3 focus:outline-none rounded text-base mt-4 md:mt-0 cursor-pointer">
                             <Link to ="/Cart">Cart</Link>
                         </li>
-                        <li className="inline-flex items-center dark:bg-gray-800 dark:text-white text-black border-0 py-1 px-3 focus:outline-none hover:bg-indigo-600 rounded text-base mt-4 md:mt-0 cursor-pointer">
+                        <li className="inline-flex items-center dark:bg-gray-800 dark:text-white text-white border-0 py-1 px-3 focus:outline-none rounded text-base mt-4 md:mt-0 cursor-pointer">
                             <Link to ="/OrderHistory">Order History</Link>
                         </li>
                     </div>
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -62,8 +62,8 @@ function Login(props) {
         ) : null}
         <div className="flex-row flex-end">
 
-          <button className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
-          type="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Submit</button>
+          <button className="inline-block px-7 py-3 text-white font-medium text-sm leading-snug uppercase rounded shadow-md  hover:shadow-lg  focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full"
+          type="text-white font-bold py-2 px-4 rounded-full">Submit</button>
 
         </div>
       </form>
